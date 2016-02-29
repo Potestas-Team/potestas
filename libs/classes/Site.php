7d8b11c98526afce50c60aff38fe43f2bd3a1a23
overflowerror <mail@overflowerror.com>
2016-02-29 22:37:45 +0100

    that name changed

--- libs/classes/Site.php
+++ libs/classes/Site.php
@@ -10,7 +10,7 @@
 		static private $sql;
 
 		static public function init() {
-			include(ROOT_LOCATION . "/config/mysql.pw.php");
+			include(ROOT_LOCATION . "/config/mysql.nogit.php");
 			Site::$sql = new mysqli($MYSQL_HOSTNAME, $MYSQL_USERNAME, $MYSQL_PASSWORD, $MYSQL_DATABASE);
 			if (Site::$sql->connect_errno) {
 				if (DEBUG)
