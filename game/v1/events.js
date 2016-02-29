5a571d23552b2791637a0b52ba360f772b907acf
overflowerror <mail@overflowerror.com>
2016-02-29 22:58:28 +0100

    well, dynamic loading will not work like this I guess

--- game/v1/events.js
+++
@@ -1,8 +0,0 @@
-var Event = function() {
-}
-Event.registerAPIEvents = function() {
-	gapi.hangout.onParticipantsChanged.add(function(eventObj) {
-	});
-	gapi.hangout.layout.getDefaultVideoFeed().onDisplayedParticipantChanged.add(function(eventObj) {
-	});
-}
