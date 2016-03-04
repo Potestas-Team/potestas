fbd5597a9b7f826ad10b8561bcda4b562cf64770
overflowerror <mail@overflowerror.com>
2016-03-04 20:15:41 +0100

    change

--- libs/classes/Site.php
+++ libs/classes/Site.php
@@ -1,10 +1,9 @@
 <?php
-	require_once(ROOT_LOCATION . "/libs/classes/ErrorHandler.php");
-	require_once(ROOT_LOCATION . "/libs/classes/SessionManager.php");
-	require_once(ROOT_LOCATION . "/libs/classes/UserManager.php");
-
 	if (!defined("ROOT_LOCATION"))
 		die("critical: root location not defined");
+
+	require_once(ROOT_LOCATION . "/libs/classes/SessionManager.php");
+	require_once(ROOT_LOCATION . "/libs/classes/UserManager.php");
 
 	class Site {
 		static private $sql;
@@ -14,9 +13,9 @@
 			Site::$sql = new mysqli($MYSQL_HOSTNAME, $MYSQL_USERNAME, $MYSQL_PASSWORD, $MYSQL_DATABASE);
 			if (Site::$sql->connect_errno) {
 				if (DEBUG)
-					ErrorHandler::unformatedError("Site", "MySQLi connection failed: " . Site::$sql->connect_error);
+					die("Site", "MySQLi connection failed: " . Site::$sql->connect_error);
 				else
-					ErrorHandler::unformatedError("Site", "There is some fatal error with the database-connection.");
+					die("Site", "There is some fatal error with the database-connection.");
 			}
 
 			Session::init();
